279db90eb644bb30525c228a9ae72cc0911c1104
234814 <234814@KB37-309-C03>
2024-09-10 13:22:52 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApplication2/ConsoleApplication2.cpp
@@ -0,0 +1,25 @@
+﻿#include<iostream>
+#include<string>
+using namespace std;
+int grob = 155555555;
+int main()
+{
+	char w[] = "west";
+	int pluh = 20;
+	string s1 = "hello";
+	char u = 's';
+	double d1 = 25;
+	double f1 = 25.6;
+	int a1, b1, c1;
+	char n = 'A';
+	int a111 = 25;
+	char h = 'H';
+	string du = "Hello";
+	char b6 = 'B';
+	int a2, a3, c3 = 18;
+	string s99 = "hi";
+	string t = "lets go";
+	int broinrt = 120;
+	cout << grob << endl << broinrt;
+	return 0;
+}
